275b755d1401d15cc5559d7946887edecd431dba
idil <idilkara5@gmail.com>
2025-02-18 04:07:53 +0300

    the user input part, switched png to svg etc

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -19,26 +19,34 @@ function InputBox({ onSendMessage, loading }) {
 
   return (
     <div className="flex justify-center p-4 min-h-20">
-      <div className="flex align-center w-1/2 bg-gray-300 rounded-full px-5 py-1">
+      <div className="flex items-center w-1/2 bg-gray-100 rounded-full px-4 py-2 shadow-sm">
         <input
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
           onKeyDown={handleKeyDown}
-          placeholder="Type here"
-          className="flex-1 border-none outline-none bg-transparent accent-pink-400"
+          placeholder="Type here..."
+          className="flex-1 bg-transparent border-none outline-none text-gray-700 text-base"
         />
         <button
-          onClick={() => handleSend()}
-          className={`rounded-full bg-gray-200 p-3 flex items-center justify-center ${
-            loading ? "cursor-pointer" : ""
-          }`}
+          onClick={handleSend}
+          className="rounded-full bg-gray-200 hover:bg-gray-300 p-2 transition"
           disabled={loading}
         >
           {loading ? (
             <span className="loading loading-spinner loading-sm"></span>
           ) : (
-            <img src="/arrow.png" alt="" className="size-4" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="w-5 h-5 text-black"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M5 15l7-7 7 7" />
+
+            </svg>
           )}
         </button>
       </div>
